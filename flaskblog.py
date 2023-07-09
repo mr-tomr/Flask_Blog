5300cffcd6955bc1d9bc1ec13242d870c457cbc8
toor <tom.reilly3@pm.me>
2023-07-08 22:45:25 -0400

    Starting with templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
-
-@app.route("/admin")
-def admin():
-    return "<h1>Admin Page</h1>"
-
+    return render_template('about.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
