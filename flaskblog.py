5f15ebf213f3aea2b9f66a6766f54f2e8081fa7a
toor <tom.reilly3@pm.me>
2023-07-09 00:08:08 -0400

    Backup of work, half through video two.

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
 
+
+posts = [
+    {
+        'author': 'Tom',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'July 8, 2023'
+    },
+
+    {
+        'author': 'Not Tom',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'July 8, 2023'
+    }
+
+]
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
         app.run(debug=True)
